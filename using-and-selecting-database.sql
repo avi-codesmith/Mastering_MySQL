3a59c49d4a450091ea6efb8c32ef7bcbef5b73aa
avi-codesmith <avi.codesmith@gmail.com>
2026-06-11 16:21:08 +0530

    Add using-and-selecting-database.sql
    Purpose: Used to select an existing database before running queries in MySQL.
    
    Syntax:
    
    USE database_name;
    
    Example:
    
    USE school;

--- using-and-selecting-database.sql
+++ using-and-selecting-database.sql
@@ -1,13 +1,3 @@
-/*USE sys;*/
-/*now we are in sys database*/
-/*we can check it by use Select database()*/
-/*SELECT DATABASE();*/ 
-/*Ofcourse we can create oth databases:*/
-/*CREATE DATABASE MobileID */
-/*but still we are in sys database*/
-/*We can jump to oth databases*/
-/*USE soapShop*/
+USE school;
+
 SELECT DATABASE();
-/*We can just double click to any database to use*/
-
-
