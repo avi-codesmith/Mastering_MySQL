8ac1b8628c61c9e554d4594ba30a81d00e38d19f
avi-codesmith <avi.codesmith@gmail.com>
2026-06-12 16:09:32 +0530

    Add droping-tables.sql
    Purpose: Used to create a new table in a MySQL database.
    
    Syntax:
    
    CREATE TABLE table_name (
      column_name data_type
    );
    
    Example:
    
    CREATE TABLE table_name (...);

---
+++ droping-tables.sql
@@ -0,0 +1,17 @@
+-- SHOW databases;
+-- USE pet_shop;
+-- SELECT database(); 
+-- SHOW tables;
+-- CREATE TABLE cowChamber (
+--  name varchar(50),
+--  colour varchar(20),
+--  breed varchar(60),
+--  number int,
+--  age int
+-- );
+
+-- SHOW TABLES; 
+
+-- DESC cowChamber; 
+DROP TABLE cowChamber;
+SHOW tables; 
