bcb7ca4fef5c8808979855e24215ffcde1654991
avi-codesmith <avi.codesmith@gmail.com>
2026-06-12 18:26:47 +0530

    Update multiple-inserts-in-table.sql
    Purpose: Used to create a new table in a MySQL database.
    
    Syntax:
    
    CREATE TABLE table_name (
      column_name data_type
    );
    
    Example:
    
    CREATE TABLE table_name (...);

--- multiple-inserts-in-table.sql
+++ multiple-inserts-in-table.sql
@@ -5,8 +5,22 @@
 -- SELECT * FROM catChamber;
 -- insert into people(age, lastname, firstname) values(2, "harry", "singh");
 -- insert into people(firstname, lastname, age) values("Ben", "Cutting", 10);
-INSERT INTO people(firstname, lastname, age)
-            values("Sam", "Vhorcho", 10), 
-				  ("Volta", "Squal", 20), 
-                  ("Mouch", "Vokka", 25);
-SELECT * FROM people;
+-- INSERT INTO people(firstname, lastname, age)
+--             values("Sam", "Vhorcho", 10), 
+-- 				  ("Volta", "Squal", 20), 
+--                   ("Mouch", "Vokka", 25);
+-- SELECT * FROM people;
+
+ /*WORKING WITH NOT NULL*/
+-- create table cow (name VARCHAR(50) NOT NULL,
+--  age INT NOT NULL);
+-- SELECT database();
+-- USE pet_shop;
+-- show TABLES;
+
+INSERT INTO cow(name, age) VALUES("BIBLOO", 20); 
+
+SELECT * FROM cow;
+-- SELECT name FROM cow; 
+DESC cow
+
