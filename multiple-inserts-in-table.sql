6c8fa55ff56c7e57be58fc9cc8ffd38448e8e9bd
avi-codesmith <avi.codesmith@gmail.com>
2026-06-12 18:01:38 +0530

    Update multiple-inserts-in-table.sql
    Purpose: Used to add new records into a MySQL table.
    
    Syntax:
    
    INSERT INTO table_name (column_name) VALUES (value);
    
    Example:
    
    INSERT INTO people(firstname, lastname, age)

--- multiple-inserts-in-table.sql
+++ multiple-inserts-in-table.sql
@@ -3,4 +3,10 @@
 -- INSERT INTO catChamber(name) value("avi", "javi", "navi
 -- insert INTO catChamber(name, age) value("avi", 2), ("ravi", 4), ("navi",9);
 -- SELECT * FROM catChamber;
-insert into people(age, lastname, firstname) values(2, "harry", "singh");
+-- insert into people(age, lastname, firstname) values(2, "harry", "singh");
+-- insert into people(firstname, lastname, age) values("Ben", "Cutting", 10);
+INSERT INTO people(firstname, lastname, age)
+            values("Sam", "Vhorcho", 10), 
+				  ("Volta", "Squal", 20), 
+                  ("Mouch", "Vokka", 25);
+SELECT * FROM people;
