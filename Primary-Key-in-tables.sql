2ffd22d48a340884ba4d7996aca267fc22303abf
avi-codesmith <avi.codesmith@gmail.com>
2026-06-13 11:23:31 +0530

    Add Primary-Key-in-tables.sql
    Purpose: Used to create a new table in a MySQL database.
    
    Syntax:
    
    CREATE TABLE table_name (
      column_name data_type
    );
    
    Example:
    
    CREATE TABLE table_name (...);

---
+++ Primary-Key-in-tables.sql
@@ -0,0 +1,39 @@
+/*UNIQUE KEYS - primary key*/
+
+-- CREATE TABLE uniqueCats3(cat_ID INT primary key, name VARCHAR(20), age INT); -- 
+-- INSERT INTO uniqueCats3(cat_ID, name, age) values(25, "pettiows", 1),(333, "pettieor", 2);
+-- DESC uniqueCats3;
+-- SELECT * FROM uniqueCats3;
+
+-- drop table uniqueCats;
+
+/*MORE ON PRIMARY KEY*/
+
+-- CREATE TABLE uniqueCats(cat_Id INT NOT NULL,
+--  name VARCHAR(20) NOT NULL DEFAULT 'NO-NAME',
+--  primary key(cat_Id)
+--  );
+
+-- INSERT INTO uniqueCats(cat_Id, name) VALUES(3, "ROWANIS"),(5, "HONRSEWAGLE");
+-- DESC uniqueCats;
+
+-- DROP TABLE uniqueCats;
+
+/*AUTO KEYS - AUTO_INCREMENT*/
+
+-- CREATE TABLE uniqueCats(cat_Id INT AUTO_INCREMENT,
+--  name VARCHAR(20) NOT NULL DEFAULT 'NO-NAME',
+--  primary key(cat_Id)
+--  );
+
+-- no need to provide ID mannualy - when we have AUTO_INCREMENT IN TABLE FOR cat_Id
+
+INSERT INTO uniqueCats(name) VALUES("Avioatroi");
+INSERT INTO uniqueCats(name) VALUES("Avioatroi");
+INSERT INTO uniqueCats(name) VALUES("Avioatroi");
+INSERT INTO uniqueCats(name) VALUES("Avioatroi");
+INSERT INTO uniqueCats(name) VALUES("Avioatroi");
+INSERT INTO uniqueCats( name) VALUES( "gaoiya");
+
+
+select * from uniqueCats;
