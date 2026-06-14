994156633bf361145544befd393c319569cc34f9
avi-codesmith <avi.codesmith@gmail.com>
2026-06-14 18:32:35 +0530

    Add string-functions.sql
    Purpose: Used to create a new table in a MySQL database.
    
    Syntax:
    
    CREATE TABLE table_name (
      column_name data_type
    );
    
    Example:
    
    CREATE TABLE books (

---
+++ string-functions.sql
@@ -0,0 +1,54 @@
+/*String functions*/
+
+CREATE DATABASE book_shop;
+USE book_shop;
+
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT,
+    title VARCHAR(100),
+    author_fname VARCHAR(100),
+    author_lname VARCHAR(100),
+    released_year INT,
+    stock_quantity INT,
+    pages INT,
+    PRIMARY KEY(book_id)
+);
+
+INSERT INTO 
+   books
+   (title, author_fname, author_lname, released_year, stock_quantity, pages)
+   VALUES
+   ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+   ('Norse Mythology', 'Neil', 'Gaiman', 2016, 43, 304),
+   ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+   ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+   ('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+   ('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+   ('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+   ('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+   ('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+   ('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+   ('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 98, 546),
+   ("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526), 
+   ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+   ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+   ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+   ('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+
+/*CONCAT - COMBINING COLUMNS (vertical one ||)*/
+
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS author_fullname FROM books;
+   SELECT CONCAT_WS('-', title, author_fname, author_lname ) AS URL FROM books;
+-- SELECT * FROM books; 
+
+/*SUBSTRING - GET A PART OF STRING (COLUMN OF A TABLE USUALLY - STRING)*/
+
+-- SELECT SUBSTRING("string or string column", starting_point, ending_point (counts from the starting point));
+SELECT SUBSTRING("HELLO WORLD", 2, 4);
+SELECT SUBSTRING("HELLO WORLD", 7); -- starts from seven end when words end but still works :D
+SELECT SUBSTRING("HELLO WORLD", -1, 2); -- negative value starts backwords
+
+/*REAL WORLD USAGE*/
+-- SUBSTRING === SUBSTR (NO DIFFERNCE)
+SELECT SUBSTR(author_fname, 1, 1) AS AVATAR, author_fname FROM books;
+ 
